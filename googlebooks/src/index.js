135e855c152bb8a6011922f5859b62f7466f9baa
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2020-03-11 00:34:30 -0700

    passing root to convert into web page

--- googlebooks/src/index.js
+++ googlebooks/src/index.js
@@ -2,6 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+//passing root to convert into web page
 ReactDOM.render(<App />, document.getElementById("root"));
 
